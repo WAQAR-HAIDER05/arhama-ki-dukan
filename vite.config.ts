3c356c0afac2efb65506aaecacfe27df802f7495
Waqar Haider <107320775+WAQAR-HAIDER05@users.noreply.github.com>
2025-08-01 23:42:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/arhama-ki-dukan/', // <-- Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
